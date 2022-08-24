68e00a368e7c78bb9c41d342544defb61518db3e
Berkay Özışık <bozisik93@hotmail.com>
2022-08-24 23:15:27 +0300

    Autofac updated.

--- NLayer.API/Modules/RepoServiceModule.cs
+++ NLayer.API/Modules/RepoServiceModule.cs
@@ -1,6 +1,12 @@
 ﻿using Autofac;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
 using NLayer.Repository;
+using NLayer.Repository.Repositories;
+using NLayer.Repository.UnitofWork;
 using NLayer.Service.Mapping;
+using NLayer.Service.Services;
 using System.Reflection;
 using Module = Autofac.Module;
 
@@ -10,11 +16,27 @@ namespace NLayer.API.Modules
     {
         protected override void Load(ContainerBuilder builder)
         {
+            //builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
+
+            //builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
+
+            //builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
+
             var apiAssembly = Assembly.GetExecutingAssembly();
             var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
             var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
 
+            //builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            //builder.Services.AddScoped<IProductService, ProductService>();
+            //builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            //builder.Services.AddScoped<ICategoryService, CategoryService>();
+
             builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
 
             base.Load(builder);
         }
